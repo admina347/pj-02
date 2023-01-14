5e42b65daee9094002050b4efc673d32b4296e63
admina <it-mactep@mail.ru>
2023-01-14 12:22:38 +0500

    dbcontext

--- EF.DataAccessLibrary/Dataaccess/LibraryContext.cs
+++ EF.DataAccessLibrary/Dataaccess/LibraryContext.cs
@@ -1,7 +1,14 @@
+
+using EF.DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Models;
+
 namespace EF.DataAccessLibrary.Dataaccess
 {
-    public class LibraryContext
+    public class LibraryContext : DbContext
     {
-        
+        public LibraryContext(DbContextOptions options) : base(options) {}
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
--- EF.DataAccessLibrary/Models/Book.cs
+++ EF.DataAccessLibrary/Models/Book.cs
@@ -4,6 +4,6 @@ namespace EF.DataAccessLibrary.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public DateOnly Date { get; set; }
+        public DateOnly PublicationDate { get; set; }
     }
 }
