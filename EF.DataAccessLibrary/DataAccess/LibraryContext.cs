cb691600a21634234eb0144be1515080087cb6e6
admina <it-mactep@mail.ru>
2023-01-21 12:41:38 +0500

    add author migrations sample data load

--- EF.DataAccessLibrary/DataAccess/LibraryContext.cs
+++ EF.DataAccessLibrary/DataAccess/LibraryContext.cs
@@ -12,5 +12,7 @@ namespace EF.DataAccessLibrary.Dataaccess
         public LibraryContext(DbContextOptions options) : base(options) {}
         public DbSet<User> Users { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Genre> Genres { get; set; }
     }
 }
--- EF.Web/Pages/Index.cshtml.cs
+++ EF.Web/Pages/Index.cshtml.cs
@@ -1,19 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text.Json;
+using EF.DataAccessLibrary.Dataaccess;
+using EF.DataAccessLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Models;
 
 namespace EF.Web.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly LibraryContext _db;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, LibraryContext db)
     {
         _logger = logger;
+        _db = db;
     }
 
     public void OnGet()
-    {
+    {   
+        //Заполним базу начальными данными - первый запуск
+        LoadSampleDataBooks();
+        LoadSampleDataUsers();
+    }
 
+    private void LoadSampleDataBooks()
+    {
+        if (_db.Books.Count() == 0)
+        {
+            string file = System.IO.File.ReadAllText("generated_books.json");
+            var book = JsonSerializer.Deserialize<List<Book>>(file);
+            _db.AddRange(book);
+            _db.SaveChanges();
+        }
+    }
+    private void LoadSampleDataUsers()
+    {
+        if (_db.Users.Count() == 0)
+        {
+            string file = System.IO.File.ReadAllText("generated_users.json");
+            var user = JsonSerializer.Deserialize<List<User>>(file);
+            _db.AddRange(user);
+            _db.SaveChanges();
+        }
     }
 }
