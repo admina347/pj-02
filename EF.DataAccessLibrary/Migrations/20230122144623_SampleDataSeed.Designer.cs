26abf8ed5399b7c8852352c417afc3b44fd370fd
admina <it-mactep@mail.ru>
2023-01-22 19:51:49 +0500

    DataSeed

--- EF.DataAccessLibrary/Migrations/20230122144623_SampleDataSeed.Designer.cs
+++ EF.DataAccessLibrary/Migrations/20230122144623_SampleDataSeed.Designer.cs
@@ -4,6 +4,7 @@ using EF.DataAccessLibrary.Dataaccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EF.DataAccessLibrary.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20230122144623_SampleDataSeed")]
+    partial class SampleDataSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
